cb09ef10599eb3ab214ef74f238718c67eb1955e
Grokher <grokherpub@gmail.com>
2021-02-03 12:49:46 +0100

    cam rotation

---
+++ Assets/Scripts/CamRotation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CamRotation : MonoBehaviour
+{
+    [Range(1, 5)] public float speed = 3.5f;
+    private float x, y;
+    public Texture2D c1, c2;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            transform.Rotate(new Vector3(Input.GetAxis("Mouse Y") * speed, -Input.GetAxis("Mouse X") * speed, 0));
+            x = transform.rotation.eulerAngles.x;
+            y = transform.rotation.eulerAngles.y;
+            transform.rotation = Quaternion.Euler(x, y, 0);
+            Cursor.SetCursor(c2, Vector2.zero, CursorMode.ForceSoftware);
+        }
+        else
+        {
+            Cursor.SetCursor(c1, Vector2.zero, CursorMode.ForceSoftware);
+        }
+    }
+}
