65cc5d6510721d5a4ff555c6a06e17df9cb44718
saltypacifist <56338657+saltypacifist@users.noreply.github.com>
2021-02-05 13:16:40 +0100

    added the creat user function still broken

---
+++ Assets/Scripts/AddUser.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddUser : MonoBehaviour
+{
+    public InputField username;
+    public InputField password;
+    public Button createButton;
+
+    public string Url;
+    WWWForm form;
+    
+    public void OnButtonclick()
+    {
+        createButton.interactable = false;
+        StartCoroutine(CreateUser());
+    }
+    IEnumerator CreateUser()
+    {
+        form = new WWWForm();
+
+        form.AddField("NewUserName", username.text);
+        form.AddField("NewUserPassword", password.text);
+        //form.AddField("NewRights", Rights.text);
+
+        WWW w = new WWW(Url,form);
+
+        yield return w;
+        if (w.error != null)
+        {
+            
+            Debug.Log("<color=red>" + w.text + "</color>");//error
+        }
+        else
+        {
+            if (w.isDone)
+            {
+                // als de echo een error woord bevat
+                if (w.text.Contains("error"))
+                {
+                   
+                    Debug.Log("<color=red>" + w.text + "</color>");//error
+                }
+                else
+                {
+                    
+                    
+                    Debug.Log(w.text);
+                }
+            }
+        }
+
+        createButton.interactable = true;
+        w.Dispose();
+    }
+    
+   
+}
