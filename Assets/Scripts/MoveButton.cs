cc7e582f2984bbd35b13b034c9e57a69e3d95afa
Grokher <grokherpub@gmail.com>
2021-02-05 14:03:52 +0100

    deleted folder + updated script

--- Assets/Scripts/MoveButton.cs
+++ Assets/Scripts/MoveButton.cs
@@ -4,91 +4,116 @@ using UnityEngine;
 
 public class MoveButton : MonoBehaviour
 {
-    enum buttonTypes
+
+    public Material[] mySkyBoxMaterial;
+
+    int currentSkyBox;
+
+    bool buttonClick = false;
+
+    public ArrayList[] structs = new ArrayList[5];
+
+    private void Update()
     {
-        moveToNextArea,
-        moveToPreviousArea
+            moveToNextArea();
     }
-    [SerializeField] buttonTypes buttonType;
 
-    GameObject UIElement;
-
-    Vector2 originalUISize;
-    Vector3 originalButtonSize;
-
-    [HideInInspector] public bool hasRun;
-
-    private void Start()
+    public void moveToNextArea()
     {
-        transform.LookAt(Camera.main.transform);
-        Raycasting.current.interactionTriggerd += HasInteracted;
-
-        SetUI(false);
-        hasRun = true;
-    }
-    private void SetUI(bool status)
-    {
-        if (UIElement != null)
+        if (buttonClick == true)
         {
-            UIElement.SetActive(status);
+            RenderSettings.skybox = mySkyBoxMaterial[1];
         }
     }
+    
 
-    private void HasInteracted(string name)
-    {
-        if (this.transform.name == name)
-        {
-            switch ((int)buttonType + 1)
-            {
-                case 1:
-                    MoveToNextArea();
-                    break;
-                case 2:
-                    MoveToPreviousArea();
-                    break;
-            }
-        }
-    }
-    public  void MoveToNextArea()
-    {
-        if (nextareaInfo.nextArea.GetComponentInChildren<MoveButton>().hasRun != true)
-        {
-            SetUI(true);
-        }
-        this.transform.parent.gameObject.SetActive(false);
-        nextareaInfo.nextArea.SetActive(true);
-        
 
-        RenderSettings.skybox = nextareaInfo.SkyBox;
-    }
 
-    public  void MoveToPreviousArea()
-    {
-        if (previousareaInfo.previousArea.GetComponentInChildren<MoveButton>().hasRun != true)
-        {
-            SetUI(true);
-        }
-        this.transform.parent.gameObject.SetActive(false);
-        previousareaInfo.previousArea.SetActive(true);
-        
+    /* enum buttonTypes
+     {
+         moveToNextArea,
+         moveToPreviousArea
+     }
+     [SerializeField] buttonTypes buttonType;
 
-        RenderSettings.skybox = previousareaInfo.skybox2;
-    }
+     GameObject UIElement;
 
-    [System.Serializable]
-    public struct NextArea
-    {
-        public GameObject nextArea;
-        public Material SkyBox;
-    }
-    [Space, SerializeField] NextArea nextareaInfo;
+     Vector2 originalUISize;
+     Vector3 originalButtonSize;
 
-    [System.Serializable]
+     [HideInInspector] public bool hasRun;
 
-    public struct PreviousArea
-    {
-        public GameObject previousArea;
-        public Material skybox2;
-    }
-    [Space, SerializeField] PreviousArea previousareaInfo;
+     private void Start()
+     {
+         transform.LookAt(Camera.main.transform);
+         Raycasting.current.interactionTriggerd += HasInteracted;
+
+         SetUI(false);
+         hasRun = true;
+     }
+     private void SetUI(bool status)
+     {
+         if (UIElement != null)
+         {
+             UIElement.SetActive(status);
+         }
+     }
+
+     private void HasInteracted(string name)
+     {
+         if (this.transform.name == name)
+         {
+             switch ((int)buttonType + 1)
+             {
+                 case 1:
+                     MoveToNextArea();
+                     break;
+                 case 2:
+                     MoveToPreviousArea();
+                     break;
+             }
+         }
+     }
+     public  void MoveToNextArea()
+     {
+         if (nextareaInfo.nextArea.GetComponentInChildren<MoveButton>().hasRun != true)
+         {
+             SetUI(true);
+         }
+         this.transform.parent.gameObject.SetActive(false);
+         nextareaInfo.nextArea.SetActive(true);
+
+
+         RenderSettings.skybox = nextareaInfo.SkyBox;
+     }
+
+     public  void MoveToPreviousArea()
+     {
+         if (previousareaInfo.previousArea.GetComponentInChildren<MoveButton>().hasRun != true)
+         {
+             SetUI(true);
+         }
+         this.transform.parent.gameObject.SetActive(false);
+         previousareaInfo.previousArea.SetActive(true);
+
+
+         RenderSettings.skybox = previousareaInfo.skybox2;
+     }
+
+     [System.Serializable]
+     public struct NextArea
+     {
+         public GameObject nextArea;
+         public Material SkyBox;
+     }
+     [Space, SerializeField] NextArea nextareaInfo;
+
+     [System.Serializable]
+
+     public struct PreviousArea
+     {
+         public GameObject previousArea;
+         public Material skybox2;
+     }
+     [Space, SerializeField] PreviousArea previousareaInfo;*/
 }
