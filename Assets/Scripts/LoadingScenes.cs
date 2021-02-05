ad97c0ea64939f746cc53775fada44245272dd25
saltypacifist <56338657+saltypacifist@users.noreply.github.com>
2021-02-05 10:51:17 +0100

    create user screen

---
+++ Assets/Scripts/LoadingScenes.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingScenes : MonoBehaviour
+{
+    public void LoadCreateScene()
+    {
+        SceneManager.LoadScene("createUserScene");
+    }
+    public void LoadLoginScene()
+    {
+        SceneManager.LoadScene("LoginScreen");
+    }
+    
+}
