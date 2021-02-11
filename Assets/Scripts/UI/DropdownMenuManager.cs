2fdf4086913d4fd3829fe434794ba525092e2393
Nathan <nathaniel99@live.nl>
2021-02-11 15:21:51 +0100

    Added my scene for UI

---
+++ Assets/Scripts/UI/DropdownMenuManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DropdownMenuManager : MonoBehaviour
+{
+    [SerializeField]
+    Image Menu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    public void OpenMenu()
+    {
+        Menu.gameObject.SetActive(true);
+        Menu.gameObject.GetComponent<Animator>().SetBool("MenuOpen", true);
+    }
+}
