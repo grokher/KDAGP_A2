7cd6577c86fe414a45f24a076b725823485763cf
Bram <bram.wolthuis@hotmail.com>
2021-02-11 14:47:33 +0100

    test for pulling images from ftp

---
+++ Assets/Scripts/CreateMaterial.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Net;
+using System.IO;
+
+public class CreateMaterial : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+#if UNITY_EDITOR
+    private void Awake()
+    {
+        string path = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Desktop);
+        path = Path.Combine(path, "Hi.png");
+        downloadWithFTP("ftp://stamgroepa2%2540mediaenvormgeving.nl@mediaenvormgeving.nl/Images/561798.jpg", path, "stamgroepa2@mediaenvormgeving.nl", "KZ93D3fB01");
+    }
+
+    private byte[] downloadWithFTP(string ftpUrl, string savePath = "", string userName = "", string password = "")
+    {
+        FtpWebRequest request = (FtpWebRequest)WebRequest.Create(new Uri(ftpUrl));
+        //request.Proxy = null;
+
+        request.UsePassive = true;
+        request.UseBinary = true;
+        request.KeepAlive = true;
+
+        //If username or password is NOT null then use Credential
+        if (!string.IsNullOrEmpty(userName) && !string.IsNullOrEmpty(password))
+        {
+            request.Credentials = new NetworkCredential(userName, password);
+        }
+
+        request.Method = WebRequestMethods.Ftp.DownloadFile;
+
+        //If savePath is NOT null, we want to save the file to path
+        //If path is null, we just want to return the file as array
+        if (!string.IsNullOrEmpty(savePath))
+        {
+            downloadAndSave(request.GetResponse(), savePath);
+            return null;
+        }
+        else
+        {
+            return downloadAsbyteArray(request.GetResponse());
+        }
+    }
+
+    byte[] downloadAsbyteArray(WebResponse request)
+    {
+        using (Stream input = request.GetResponseStream())
+        {
+            byte[] buffer = new byte[16 * 1024];
+            using (MemoryStream ms = new MemoryStream())
+            {
+                int read;
+                while (input.CanRead && (read = input.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    ms.Write(buffer, 0, read);
+                }
+                return ms.ToArray();
+            }
+        }
+    }
+
+    void downloadAndSave(WebResponse request, string savePath)
+    {
+        Stream reader = request.GetResponseStream();
+
+        //Create Directory if it does not exist
+        if (!Directory.Exists(Path.GetDirectoryName(savePath)))
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(savePath));
+        }
+
+        FileStream fileStream = new FileStream(savePath, FileMode.Create);
+
+
+        int bytesRead = 0;
+        byte[] buffer = new byte[2048];
+
+        while (true)
+        {
+            bytesRead = reader.Read(buffer, 0, buffer.Length);
+
+            if (bytesRead == 0)
+                break;
+
+            fileStream.Write(buffer, 0, bytesRead);
+        }
+        fileStream.Close();
+    }
+
+
+#endif
+}
