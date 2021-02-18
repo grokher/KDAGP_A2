fea0e8cf4830863c069d652fc9dcadbcfde55fbc
Bram <bram.wolthuis@hotmail.com>
2021-02-18 10:55:10 +0100

    FTP upload test 1
    works :)

---
+++ Assets/Scripts/UploadTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Net;
+using System.IO;
+
+public class UploadTest : MonoBehaviour
+{
+
+    public string FTPHost = "ftp://stamgroepa2%2540mediaenvormgeving.nl@mediaenvormgeving.nl/Images/Skyboxes";
+
+    public string FTPUserName = "stamgroepa2@mediaenvormgeving.nl";
+
+    public string FTPPassword = "KZ93D3fB01";
+
+    public string FilePath;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Upload();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Upload()
+    {
+        WebClient client = new System.Net.WebClient();
+
+        Uri uri = new Uri(FTPHost + "/" + new FileInfo(FilePath).Name);
+
+        client.Credentials = new System.Net.NetworkCredential(FTPUserName, FTPPassword);
+
+        client.UploadFileAsync(uri, "STOR", FilePath);
+    }
+}
