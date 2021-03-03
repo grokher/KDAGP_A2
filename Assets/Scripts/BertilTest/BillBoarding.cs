8ace87dff61aa733c0cf7c78db048f85355a4c6c
Bertil poelman <56338911+FueeNoises@users.noreply.github.com>
2021-03-03 12:24:37 +0100

    Placer
    v1 van de question placer in mijn scene :P

---
+++ Assets/Scripts/BertilTest/BillBoarding.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillBoarding : MonoBehaviour
+{
+    Camera cam;
+    public Transform camTransform;
+
+    Quaternion originalRotation;
+
+    void Start()
+    {
+        cam = Camera.main;
+        originalRotation = transform.rotation;
+    }
+
+    void Update()
+    {
+        camTransform = cam.gameObject.transform;
+        transform.rotation = camTransform.rotation * originalRotation;
+    }
+}
