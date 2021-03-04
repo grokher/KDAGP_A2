ffb2eef1867a5ccd1756ea812fb0fb64d89cde53
saltypacifist <56338657+saltypacifist@users.noreply.github.com>
2021-03-04 09:30:38 +0100

    Update GetUserList.cs
    david good luck with this XD

--- Assets/Scripts/JordanScript/GetUserList.cs
+++ Assets/Scripts/JordanScript/GetUserList.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GetUserList : MonoBehaviour
 {
     public string URL;
-
-    List<string> UsersInfo = new List<string>();
+    WWWForm form;
+    public Text userdata;
+    
+    private List<string> UsersData = new List<string>();
 
     void Awake()
     {
+
         StartUpUserList();
     }
 
@@ -21,17 +25,35 @@ public class GetUserList : MonoBehaviour
     public void StartUpUserList()
     {
         StartCoroutine(GetUsersList());
-    }
 
+    }
+    
     IEnumerator GetUsersList()
     {
+       
         WWW w = new WWW(URL);
 
-        yield return w;
 
-        
-        Debug.Log(w.text);
-        Debug.Log(w.error);
+        UsersData.Add(w.ToString());
+            yield return w;
+
+
+        if (w.isDone)
+        {
+           
+            if (w.text.Contains("Error"))
+            {
+                Debug.Log(w.error);
+            }
+            else
+            {
+                
+            }
+
+            Debug.Log(w.text[0][0]);
+
+        }
+
 
         w.Dispose();
     }
