602704d08be63e1b0a3c3172576ec802dc21d298
SmallEnderman <likeaendermanplaysminecraft@gmail.com>
2021-03-05 10:58:39 +0100

    Updated KeepInfo (works now)

--- Assets/Scripts/JordanScript/KeepInfo.cs
+++ Assets/Scripts/JordanScript/KeepInfo.cs
@@ -1,23 +1,29 @@
 ﻿using System;
+using System.Runtime.Serialization.Json;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class KeepInfo : MonoBehaviour
 {
-    public static Object JsonConvertor(WWW w)
+    public static void JsonConvertor(WWW w)
     {
         Debug.Log(w.text);
-        return JsonUtility.FromJson<Object>(w.text);
+        string tester = w.text;
+        tester = tester.Substring(1, tester.Length - 2);
+        Debug.Log(tester);
+        Object test = new Object();
+        test = JsonUtility.FromJson<Object>(tester);
+        Debug.Log(test.isBlocked);
     }
 }
 
 [System.Serializable]
 public class Object
 {
-    public string Username;
-    public string Password;
-    public int Id;
-    public string Role;
-    public string IsBlocked;
+    public string username;
+    public string password;
+    public int id;
+    public string rights;
+    public string isBlocked;
 }
