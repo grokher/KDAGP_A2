f32cb6cb7977450752bcf0d1b3258fb87ec063f1
Bertil poelman <56338911+TheStickmenn@users.noreply.github.com>
2021-03-08 13:51:44 +0100

    Quick lil push
    Update in my scene and made pref of nathans menu

---
+++ Assets/Scripts/BertilTest/BuildingMode.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingMode : MonoBehaviour
+{
+    Canvas buildingWindow;
+
+    private void Start()
+    {
+        buildingWindow = this.gameObject.GetComponent<Canvas>();
+
+        SetWindow();
+    }
+
+    public void SetWindow()
+    {
+        buildingWindow.enabled = !buildingWindow.enabled;
+    }
+}
