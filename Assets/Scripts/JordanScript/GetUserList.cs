4b4bed4203fd520790a04db3908cc51dfdaa7c37
Grokher <grokherpub@gmail.com>
2021-03-08 14:29:14 +0100

    added system.data.sql + userdata naar list

--- Assets/Scripts/JordanScript/GetUserList.cs
+++ Assets/Scripts/JordanScript/GetUserList.cs
@@ -2,13 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Data;
+using System.Data.SqlClient;
+
 
 public class GetUserList : MonoBehaviour
 {
     public string URL;
     public Text userdata;
-    
-    private List<string> UsersData = new List<string>();
+   
+
+    private static void CreateCommand(string queryString, string connectionString)
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            SqlCommand command = new SqlCommand("SELECT username, id,isBlocked FROM users ORDER BY isBlocked");
+
+            command.Connection.Open();
+
+            SqlDataReader reader = command.ExecuteReader();
+            List<Userinfo> UsersData = new List<Userinfo>();
+            while (reader.Read())
+            {
+                Userinfo U = new Userinfo();
+                U.username = (string) reader["username"];
+                U.id = (string)reader["id"];
+                U.isBlocked = (string)reader["isBlocked"];
+                UsersData.Add(U);
+            }
+
+            command.ExecuteNonQuery();
+        }
+    } 
 
     void Awake()
     {
@@ -36,25 +62,40 @@ public class GetUserList : MonoBehaviour
             yield return w;
 
 
-        if (w.isDone)
-        {
-           
-            if (w.text.Contains("Error"))
+            if (w.isDone)
             {
-                Debug.Log(w.error);
-            }
-            else
-            {
-                userdata.text = w.text;
-                KeepInfo.JsonConvertor(w);
-                
-            }
 
-            //Debug.Log(w.text[0][0]);
+                if (w.text.Contains("Error"))
+                {
+                    Debug.Log(w.error);
+                }
+                else
+                {
+                    /*Userinfo userinfo = new Userinfo();
+                    userinfo = JsonUtility.FromJson<Userinfo>(w.text);*/
+                    foreach (string username in UsersData)
+                    {
 
-        }
+                    }
+                    userdata.text = w.text;
+
+                    //JsonUtility.FromJson<object>(w.text);
+                    //KeepInfo.JsonConvertor(w);
+
+                }
+
+                //Debug.Log(w.text[0][0]);
+
+            }
 
 
         w.Dispose();
     }
+}
+
+public class Userinfo
+{
+    public string username { set; get; }
+    public string id { set; get; }
+    public string isBlocked { set; get; }
 }
