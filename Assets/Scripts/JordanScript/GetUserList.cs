26a84843c34237cbd5e1c300f3ed2ccfa059b99f
SmallEnderman <likeaendermanplaysminecraft@gmail.com>
2021-03-11 21:00:45 +0100

    Fixed GetUserInfo

--- Assets/Scripts/JordanScript/GetUserList.cs
+++ Assets/Scripts/JordanScript/GetUserList.cs
@@ -5,18 +5,13 @@ using UnityEngine.UI;
 using System.Data;
 using System.Data.SqlClient;
 using System;
+using Newtonsoft.Json;
 
 
 public class GetUserList : MonoBehaviour
 {
     public string URL;
     public Text userdata;
-
-    void Awake()
-    {
-        
-        
-    }
 
     void Start()
     {
@@ -30,38 +25,37 @@ public class GetUserList : MonoBehaviour
         WWW w = new WWW(URL);
 
         
-            yield return w;
+        yield return w;
 
 
-            if (w.isDone)
+        if (w.isDone)
+        {
+            if (w.error != null)
             {
-                if (w.error != null)
-                {
-                    Debug.Log(w.error);
-                }
-                else
-                {
-                    Debug.Log(w.text);
-                    Userinfo userinfo = new Userinfo();
-                    userinfo = JsonUtility.FromJson<Userinfo>(w.text);
-                    
-                    userdata.text = w.text;
-                    
-                    //JsonUtility.FromJson<object>(w.text);
-                    //KeepInfo.JsonConvertor(w);
+                Debug.Log(w.error);
+            }
+            else
+            {
+                List<Userinfo> userinfo = new List<Userinfo>();
+                userinfo = JsonConvert.DeserializeObject<List<Userinfo>>(w.text);
 
+                foreach (var user in userinfo)
+                {
+                    //MAAK GAMEOBJECT VOOR ELKE USER, GEEF HIER ID, USERNAME EN BLOCKED DOOR
+                    //fucking dfhdshgjkdshgjedgfesdjg
+
+                    //Debug.Log("Name is " + user.username + " and Id is " + user.id);
                 }
 
-                //Debug.Log(w.text[0][0]);
 
             }
-
-
+        }
         w.Dispose();
     }
 
    
 }
+
 [Serializable]
 public class Userinfo
 {
@@ -69,44 +63,3 @@ public class Userinfo
     public string id { set; get; }
     public string isBlocked { set; get; }
 }
-/* private static void CreateCommand(string queryString, string connectionString)
-    {
-        Debug.Log("first");
-        SqlConnection conn = new SqlConnection();
-        conn.ConnectionString =
-          "Data Source=localhost;" +
-          "Initial Catalog=deb47292_stamgroepa2;" +
-          "User id=deb47292_stamgroepa2;" +
-          "Password=KZ93D3fB01;";
-        conn.Open();
-
-        Debug.Log("seconds");
-
-        using (SqlConnection connection = new SqlConnection(connectionString))
-        {
-            SqlCommand command = new SqlCommand("SELECT username, id,isBlocked FROM users ORDER BY isBlocked", conn);
-
-            command.Connection.Open();
-
-            SqlDataReader reader = command.ExecuteReader();
-            List<Userinfo> UsersData = new List<Userinfo>();
-            while (reader.Read())
-            {
-                Userinfo U = new Userinfo();
-                U.username = (string)reader["username"];
-                U.id = (string)reader["id"];
-                U.isBlocked = (string)reader["isBlocked"];
-                UsersData.Add(U);
-            }
-            foreach (Userinfo U in UsersData)
-            {
-                Debug.Log(U);
-            }
-
-            command.ExecuteNonQuery();
-
-            Debug.Log(UsersData);
-            
-        }
-
-    }*/
