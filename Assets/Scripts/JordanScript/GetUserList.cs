e653c1ebe433e6b81b72658f64b61d4cf02ab8ae
Grokher <grokherpub@gmail.com>
2021-03-11 10:06:56 +0100

    updating script so there is no error

--- Assets/Scripts/JordanScript/GetUserList.cs
+++ Assets/Scripts/JordanScript/GetUserList.cs
@@ -11,13 +11,22 @@ public class GetUserList : MonoBehaviour
     public string URL;
     public Text userdata;
    
-
     private static void CreateCommand(string queryString, string connectionString)
     {
+        Debug.Log("first");
+        SqlConnection conn = new SqlConnection();
+        conn.ConnectionString =
+          "Data Source=localhost;" +
+          "Initial Catalog=deb47292_stamgroepa2;" +
+          "User id=deb47292_stamgroepa2;" +
+          "Password=KZ93D3fB01;";
+        conn.Open();
+
+        Debug.Log("seconds");
+
         using (SqlConnection connection = new SqlConnection(connectionString))
         {
-            connection.Open();
-            SqlCommand command = new SqlCommand("SELECT username, id,isBlocked FROM users ORDER BY isBlocked");
+            SqlCommand command = new SqlCommand("SELECT username, id,isBlocked FROM users ORDER BY isBlocked", conn);
 
             command.Connection.Open();
 
@@ -33,13 +42,16 @@ public class GetUserList : MonoBehaviour
             }
 
             command.ExecuteNonQuery();
+            
+            Debug.Log(UsersData);
+
         }
     } 
 
     void Awake()
     {
-
-        StartUpUserList();
+        
+        
     }
 
     void Start()
@@ -73,12 +85,12 @@ public class GetUserList : MonoBehaviour
                 {
                     /*Userinfo userinfo = new Userinfo();
                     userinfo = JsonUtility.FromJson<Userinfo>(w.text);*/
-                    foreach (string username in UsersData)
+                    /*foreach (string username in UsersData)
                     {
 
                     }
                     userdata.text = w.text;
-
+                    */
                     //JsonUtility.FromJson<object>(w.text);
                     //KeepInfo.JsonConvertor(w);
 
