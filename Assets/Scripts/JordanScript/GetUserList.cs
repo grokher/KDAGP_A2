9736683a84564fedfe9750fb5fce65ee04ab21e8
Grokher <grokherpub@gmail.com>
2021-03-11 13:49:48 +0100

    push for jord

--- Assets/Scripts/JordanScript/GetUserList.cs
+++ Assets/Scripts/JordanScript/GetUserList.cs
@@ -4,50 +4,13 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Data;
 using System.Data.SqlClient;
+using System;
 
 
 public class GetUserList : MonoBehaviour
 {
     public string URL;
     public Text userdata;
-   
-    private static void CreateCommand(string queryString, string connectionString)
-    {
-        Debug.Log("first");
-        SqlConnection conn = new SqlConnection();
-        conn.ConnectionString =
-          "Data Source=localhost;" +
-          "Initial Catalog=deb47292_stamgroepa2;" +
-          "User id=deb47292_stamgroepa2;" +
-          "Password=KZ93D3fB01;";
-        conn.Open();
-
-        Debug.Log("seconds");
-
-
-        using (SqlConnection connection = new SqlConnection(connectionString))
-        {
-            SqlCommand command = new SqlCommand("SELECT username, id,isBlocked FROM users ORDER BY isBlocked", conn);
-
-            command.Connection.Open();
-
-            SqlDataReader reader = command.ExecuteReader();
-            List<Userinfo> UsersData = new List<Userinfo>();
-            while (reader.Read())
-            {
-                Userinfo U = new Userinfo();
-                U.username = (string) reader["username"];
-                U.id = (string)reader["id"];
-                U.isBlocked = (string)reader["isBlocked"];
-                UsersData.Add(U);
-            }
-
-            command.ExecuteNonQuery();
-            
-            Debug.Log(UsersData);
-
-        }
-    } 
 
     void Awake()
     {
@@ -57,41 +20,33 @@ public class GetUserList : MonoBehaviour
 
     void Start()
     {
-
-    }
-
-    public void StartUpUserList()
-    {
         StartCoroutine(GetUsersList());
-
     }
+
     
     IEnumerator GetUsersList()
     {
        
         WWW w = new WWW(URL);
 
-        //UsersData.Add(w.ToString());
+        
             yield return w;
 
 
             if (w.isDone)
             {
-
-                if (w.text.Contains("Error"))
+                if (w.error != null)
                 {
                     Debug.Log(w.error);
                 }
                 else
                 {
-                    /*Userinfo userinfo = new Userinfo();
-                    userinfo = JsonUtility.FromJson<Userinfo>(w.text);*/
-                    /*foreach (string username in UsersData)
-                    {
-
-                    }
+                    Debug.Log(w.text);
+                    Userinfo userinfo = new Userinfo();
+                    userinfo = JsonUtility.FromJson<Userinfo>(w.text);
+                    
                     userdata.text = w.text;
-                    */
+                    
                     //JsonUtility.FromJson<object>(w.text);
                     //KeepInfo.JsonConvertor(w);
 
@@ -104,11 +59,54 @@ public class GetUserList : MonoBehaviour
 
         w.Dispose();
     }
-}
 
+   
+}
+[Serializable]
 public class Userinfo
 {
     public string username { set; get; }
     public string id { set; get; }
     public string isBlocked { set; get; }
 }
+/* private static void CreateCommand(string queryString, string connectionString)
+    {
+        Debug.Log("first");
+        SqlConnection conn = new SqlConnection();
+        conn.ConnectionString =
+          "Data Source=localhost;" +
+          "Initial Catalog=deb47292_stamgroepa2;" +
+          "User id=deb47292_stamgroepa2;" +
+          "Password=KZ93D3fB01;";
+        conn.Open();
+
+        Debug.Log("seconds");
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            SqlCommand command = new SqlCommand("SELECT username, id,isBlocked FROM users ORDER BY isBlocked", conn);
+
+            command.Connection.Open();
+
+            SqlDataReader reader = command.ExecuteReader();
+            List<Userinfo> UsersData = new List<Userinfo>();
+            while (reader.Read())
+            {
+                Userinfo U = new Userinfo();
+                U.username = (string)reader["username"];
+                U.id = (string)reader["id"];
+                U.isBlocked = (string)reader["isBlocked"];
+                UsersData.Add(U);
+            }
+            foreach (Userinfo U in UsersData)
+            {
+                Debug.Log(U);
+            }
+
+            command.ExecuteNonQuery();
+
+            Debug.Log(UsersData);
+            
+        }
+
+    }*/
