5752e10f5700155c8b8fc71210a5928b220bea95
Nathan <nathaniel99@live.nl>
2021-03-12 10:44:04 +0100

    Location of inputFieldSwitcher changed

---
+++ Assets/Scripts/inputFieldSwitcher.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class inputFieldSwitcher : MonoBehaviour
+{
+    [SerializeField]InputField[] fields;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            for (int i = 0; i < fields.Length; i++)
+            {                
+                if (fields[i].isActiveAndEnabled)
+                {
+                    Debug.Log((i).ToString());
+                    if ((i + 2) > fields.Length)
+                    {
+                        Debug.Log("Field is:" + (i + 2).ToString());
+                        Debug.Log("Length is:" + fields.Length.ToString());
+                        fields[0].ActivateInputField();
+                    }
+                    if ((i + 2) <= fields.Length)
+                    {
+                        Debug.Log("This is" + (i + 1).ToString());
+                        fields[i + 1].ActivateInputField();
+                    }
+                    
+                    return;
+                }
+            }
+        }    
+    }
+}
