0256e8985816827e1c3e9a4e02b01f74460464fb
Nathan <nathaniel99@live.nl>
2021-03-12 14:19:51 +0100

    Made question box

---
+++ Assets/Scripts/QuestionsInsert.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestionsInsert : MonoBehaviour
+{
+    public string URL;
+    public Text question;
+    WWWForm form;
+
+
+    public void idk()
+    {
+        StartCoroutine(GetUsersList());
+    }
+
+
+    IEnumerator GetUsersList()
+    {
+        form = new WWWForm();
+        form.AddField("Question", question.text);
+
+        WWW w = new WWW(URL, form);
+
+
+        yield return w;
+        if (w.error != null)
+        {
+            Debug.Log(w.error);
+        }
+        if (w.isDone)
+        {
+            Debug.Log(w.text);
+        }
+        w.Dispose();
+    }
+}
