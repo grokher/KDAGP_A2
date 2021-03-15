ee208a0cd29a3f7310793922e22a6db3f1004c11
Bertil poelman <56338911+FueeNoises@users.noreply.github.com>
2021-03-15 14:15:19 +0100

    More shit for the builder
    Added some stuff for the builder and some other crap. Clent up some code. eeeeeh yeah idk figure it out urself

--- Assets/Scripts/BertilTest/QuestionPlacer.cs
+++ Assets/Scripts/BertilTest/QuestionPlacer.cs
@@ -1,27 +1,64 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class QuestionPlacer : MonoBehaviour
 {
-    bool placeQuest = false;
-    public Camera camera;
+    public GameObject[] questionTypes;
+    Quaternion rot;
+    Vector3 worldPosition;
+    bool buildingMode;
+
+    int qN;
+
+    InputField sizeField;
+    int awnsers;
+
+    Dropdown format;
 
     void Start()
     {
-        
+        rot = this.gameObject.transform.rotation;
+        format = GameObject.Find("FormatSelecor").GetComponent<Dropdown>();
+        sizeField = GameObject.Find("AwnserAmount").GetComponent<InputField>();
     }
-    
-    void Update()
+
+    public void MakeQuestion()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse1))
+        ManageQuestionFormat();
+    }
+
+    void ManageQuestionFormat()
+    {
+        try
         {
-            Debug.Log(camera.ScreenToWorldPoint(Input.mousePosition));
+            awnsers = int.Parse(sizeField.text);
+        }
+        catch (Exception e)
+        {
+            Debug.LogWarning("Hmmmm it seems some idiot isnt using A NUMBER WHAT THE FUCK YOU BUCKET");
         }
     }
 
-    public void EnablePlace()
+    void OnClick()
     {
-        placeQuest = true;
+        if (Input.GetKey(KeyCode.Mouse0) && buildingMode && qN != 0)
+        {
+            PlaceObjectAtMouse(questionTypes[qN]);
+        }
+    }
+
+    void PlaceObjectAtMouse(GameObject placeObj)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitData;
+
+        if (Physics.Raycast(ray, out hitData, 1000))
+        {
+            worldPosition = hitData.point;
+            Instantiate(placeObj, worldPosition, rot);
+        }
     }
 }
