76cded8198286b4f6b4bee396505452d85c4367c
Nathan <nathaniel99@live.nl>
2021-03-15 11:23:41 +0100

    Simplified student and editor buttons

--- Assets/Scripts/JordanScript/FilterButtons.cs
+++ Assets/Scripts/JordanScript/FilterButtons.cs
@@ -1,40 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FilterButtons : MonoBehaviour
 {
-    public List<GameObject> StudentButtons = new List<GameObject>();
-    public List<GameObject> OtherButtons = new List<GameObject>();
-
+    public List<GameObject> Buttons = new List<GameObject>();
+    public List<string> studentButtons = new List<string>();
+    public List<string> otherButtons = new List<string>();
+    public List<Button.ButtonClickedEvent> studentEvents = new List<Button.ButtonClickedEvent>();
+    public List<Button.ButtonClickedEvent> otherEvents = new List<Button.ButtonClickedEvent>();
     private void Awake()
     {
-        foreach(GameObject button in StudentButtons)
-        {
-            button.SetActive(false);
-        }
-
         if (KeepInfo.loginInfo.rights == "Student")
         {
-            foreach(GameObject button in StudentButtons)
+            for (int i = 0; i < Buttons.Count; i++)
             {
-                button.SetActive(true);
-            }
-            foreach(GameObject button in OtherButtons)
+                Buttons[i].GetComponentInChildren<Text>().text = studentButtons[i];
+                Buttons[i].GetComponent<Button>().onClick = studentEvents[i];
+            }           
+        }
+        else
+        {            
+            for (int i = 0; i < Buttons.Count; i++)
             {
-                button.SetActive(false);
+                Buttons[i].GetComponentInChildren<Text>().text = otherButtons[i];
+                Buttons[i].GetComponent<Button>().onClick = otherEvents[i];
+            }           
+        }
+    }
+    /*void ButtonFunction()
+    {
+        if (KeepInfo.loginInfo.rights == "Student")
+        {
+            for (int i = 0; i < Buttons.Count; i++)
+            {
+                Buttons[i].GetComponentInChildren<Text>().text = studentButtons[i];
+                Buttons[i].GetComponent<Button>().onClick = buttonEvents[i];
             }
         }
         else
         {
-            foreach (GameObject button in StudentButtons)
+            for (int i = 3; i < Buttons.Count; i++)
             {
-                button.SetActive(false);
-            }
-            foreach (GameObject button in OtherButtons)
-            {
-                button.SetActive(true);
+                Buttons[i].GetComponentInChildren<Text>().text = otherButtons[i];
+                Buttons[i].GetComponent<Button>().onClick = buttonEvents[i];
             }
         }
-    }
+    }*/
 }
