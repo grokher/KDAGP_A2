254f1c1d13868a91ef7c7853a9d66510aef06f53
Grokher <grokherpub@gmail.com>
2021-03-24 10:56:57 +0100

    .

--- Assets/Scripts/Database Scripts/GetUserList.cs
+++ Assets/Scripts/Database Scripts/GetUserList.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System;
-using Newtonsoft.Json;
+//using Newtonsoft.Json;
 using TMPro;
 
 
@@ -14,7 +14,6 @@ public class GetUserList : MonoBehaviour
     private Vector3 scaleChange;
 
 
-
     void Start()
     {
         StartCoroutine(GetUsersList());
@@ -22,10 +21,8 @@ public class GetUserList : MonoBehaviour
 
     private void Update()
     {
-        
     }
 
- 
 
     IEnumerator GetUsersList()
     {
@@ -42,13 +39,19 @@ public class GetUserList : MonoBehaviour
             else
             {
                 //picking up data from json file
-                List<Userinfo> userinfo = new List<Userinfo>();
-                userinfo = JsonConvert.DeserializeObject<List<Userinfo>>(w.text);
-                Debug.Log(w.text);
-                foreach (var user in userinfo)
+                List<Userinfo> userinfo = new List<Userinfo>();//Hebben jullie deze dan nog wel nodig?
+                string trimmedText = w.text.Remove(w.text.Length - 2, 2).Remove(0, 2);
+
+                string[] userinfoPieces = trimmedText.Split(new[] { "},{" }, StringSplitOptions.None);
+                for (int i = 0; i < userinfoPieces.Length; i++)
                 {
+                    Userinfo newUserInfo = new Userinfo(userinfoPieces[i]);
+                    userinfo.Add(newUserInfo);
+
+                    //Debug.Log(newUserInfo);
+
                     scaleChange = new Vector3(1, 1, 1);
-                  
+
                     GameObject parent = GameObject.Find("Content");
                     //instanitiate and transforming scale so it's right
                     GameObject textMesh = Instantiate(textdata, parent.transform);
@@ -59,13 +62,14 @@ public class GetUserList : MonoBehaviour
                     //adding data to the text
 
                     //textMesh.GetComponent<TextMeshProUGUI>().text = user.username + "  " + user.isBlocked;
-                    textMesh.GetComponentInChildren<TextMeshProUGUI>().text = user.username + "  " + user.isBlocked;
+                    textMesh.GetComponentInChildren<TextMeshProUGUI>().text = newUserInfo.username + "  " + newUserInfo.isBlocked;
 
-                    textMesh.transform.GetChild(0).transform.GetChild(0).GetComponent<Text>().text = user.isBlocked;
-                    textMesh.transform.GetChild(0).transform.GetChild(1).GetComponent<Text>().text = user.username;
+                    textMesh.transform.GetChild(0).transform.GetChild(0).GetComponent<Text>().text = newUserInfo.isBlocked;
+                    textMesh.transform.GetChild(0).transform.GetChild(1).GetComponent<Text>().text = newUserInfo.username;
                 }
             }
         }
+
         w.Dispose();
     }
 }
@@ -75,4 +79,40 @@ public class Userinfo
     public string username { set; get; }
     public string id { set; get; }
     public string isBlocked { set; get; }
+
+
+    public Userinfo(string pJsonText)
+    {
+        string[] dataPairs = pJsonText.Split(',');
+
+        for (int i = 0; i < dataPairs.Length; i++)
+        {
+            string[] keyValuePair = dataPairs[i].Split(':');
+            Debug.Assert(keyValuePair.Length == 2);
+
+            string key = keyValuePair[0];
+            string value = keyValuePair[1];
+
+            switch (key)
+            {
+                case "\"username\"":
+                    Debug.Log("Assigning username");
+                    username = value;
+                    break;
+                case "\"id\"":
+                    Debug.Log("Assigning id");
+                    id = value;
+                    break;
+                case "\"isBlocked\"":
+                    Debug.Log("Assigning isBlocked");
+                    isBlocked = value;
+                    break;
+            }
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"username: {username}, id: {id}, isBlocked: {isBlocked}";
+    }
 }
