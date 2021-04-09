deb6523b8724b390d5dd96cff4105c4742ede454
SmallEnderman <likeaendermanplaysminecraft@gmail.com>
2021-04-09 11:14:42 +0200

    student issue fixed

---
+++ Assets/Scripts/JordanScript/MainMenuRole.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuRole : MonoBehaviour
+{
+    public GameObject ToetsMenu;
+    public GameObject Bewerker;
+
+    void Start()
+    {
+        if (KeepInfo.loginInfo.rights == "Student")
+        {
+            ToetsMenu.SetActive(false);
+            Bewerker.SetActive(false);
+        }
+    }
+
+}
